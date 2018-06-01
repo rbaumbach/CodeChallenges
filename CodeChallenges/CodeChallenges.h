47b80de89c5e07b64abe353c545b107d7a53bd13
Ryan <github@ryan.codes>
2018-06-01 16:00:07 -0600

    Initial project commit

---
+++ CodeChallenges/CodeChallenges.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for CodeChallenges.
+FOUNDATION_EXPORT double CodeChallengesVersionNumber;
+
+//! Project version string for CodeChallenges.
+FOUNDATION_EXPORT const unsigned char CodeChallengesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CodeChallenges/PublicHeader.h>
---
+++ Podfile
@@ -0,0 +1,13 @@
+platform :ios, '11.0'
+
+use_frameworks!
+inhibit_all_warnings!
+
+target 'CodeChallenges' do
+  pod 'SwiftLint'
+end
+
+target 'Specs' do
+  pod 'Quick'
+  pod 'Nimble'
+end
